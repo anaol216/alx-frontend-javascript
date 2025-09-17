9288cc90025ec7fbc11cb3ab716af440084e86e8
anaol216 <atinafuanaol@gmail.com>
2025-09-17 10:13:08 +0300

    modified:   0x04-TypeScript/task_2/js/main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,13 +1,10 @@
 
 export type Subjects = 'Math' | 'History';
-export function teachClass(todayClass: Subjects): string {
+export function teachClass(todayClass: Subjects): string{
     if (todayClass === 'Math') {
         return 'Teaching Math';
-    } else if (todayClass === 'History') {
+    }
         return 'Teaching History';
-    } else {
-        return '';
-    }  
 }
 
 console.log(teachClass('Math')); // Teaching Math
