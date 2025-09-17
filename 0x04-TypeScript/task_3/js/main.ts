7c081b82cdaf60a627d4562868fc460247bcf98f
anaol216 <atinafuanaol@gmail.com>
2025-09-17 13:12:56 +0300

    modified:   0x04-TypeScript/task_3/js/crud.d.ts renamed:    0x04-TypeScript/task_3/js/crude.ts -> 0x04-TypeScript/task_3/js/crud.js modified:   0x04-TypeScript/task_3/js/main.ts modified:   0x04-TypeScript/task_3/tsconfig.json

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -8,7 +8,7 @@ const row: RowElement = {
 }
 
 const newRowID: RowID = CRUD.insertRow(row);
-const updatedRow: RowElement = {age: 23, ...row};
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
